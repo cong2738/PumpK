bfd662f19eb371b7c8fd1a8bd7fd9c9af47cc3df
cong2738 <cong2738@gmail.com>
2022-11-02 16:35:46 +0900

    백준 직사각형 면적구하기 풀어버림
    이것은 정답이란다. 주석이 내가푼건데 역겨워서 수정함

--- C++/백준/직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기/main.cpp
+++ C++/백준/직사각형 네개의 합집합의 면적 구하기/직사각형 네개의 합집합의 면적 구하기/main.cpp
@@ -1,42 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-	int area = 0;
-	vector<vector<int>> squars(4,vector<int>(4));
+	vector<int> p1(2), p2(2);
+	vector<vector<int>> st(100,vector<int>(100));
 	for (size_t i = 0; i < 4; i++)
 	{
-		for (size_t j = 0; j < 4; j++)
+		for (size_t j = 0; j < 2; j++)
 		{
-			cin>>squars[i][j];
+			cin>>p1[j];
 		}
-		area += (squars[i][2] - squars[i][0]) * (squars[i][3] - squars[i][1]);
-	}
-	vector<bool> v(4 - 2, false);
-	v.insert(v.end(), 2, true);
-	do {
-		vector<int>sel;
-		for (int k = 0; k < 4; k++) {
-			if (v[k]) {
-				sel.push_back(k);
-				cout << k << ' ';
+		for (size_t j = 0; j < 2; j++)
+		{
+			cin>>p2[j];
+		}
+		for (int x = p1[0]; x < p2[0]; x++)
+		{
+			for (int y = p1[1]; y < p2[1]; y++)
+			{
+				st[x][y] = 1;
 			}
 		}
-		cout << endl;
-		int i = sel[0]; int j = sel[1];
-		vector<int> xp = { squars[i][0], squars[i][2], squars[j][0], squars[j][2] };
-		vector<int> yp = { squars[i][1], squars[i][3], squars[j][1], squars[j][3] };
-		int XL = squars[i][2] - squars[i][0] + squars[j][2] - squars[j][0];
-		int YL = squars[i][3] - squars[i][1] + squars[j][3] - squars[j][1];
-		int bigXL = *max_element(xp.begin(), xp.end()) - *min_element(xp.begin(), xp.end());
-		int bigYL = *max_element(yp.begin(), yp.end()) - *min_element(yp.begin(), yp.end());
-		if (XL > bigXL && YL > bigYL) {
-			sort(xp.begin(), xp.end());
-			sort(yp.begin(), yp.end());
-			area -= (xp[2] - xp[1]) * (yp[2] - yp[1]);
-			cout << (xp[2] - xp[1]) * (yp[2] - yp[1]) << endl;
-		}
-	} while (next_permutation(v.begin(), v.end()));
-			
+	}
+	int area = 0;
+	for (auto i : st)for (auto j : i) area += j;
 	cout << area;
 	return 0;
 }
+
+//int main() {
+//	int area = 0;
+//	vector<vector<int>> squars(4,vector<int>(4));
+//	for (size_t i = 0; i < 4; i++)
+//	{
+//		for (size_t j = 0; j < 4; j++)
+//		{
+//			cin>>squars[i][j];
+//		}
+//	}
+//	int minX = squars[0][0], minY = squars[0][1], maxX = squars[0][2], maxY = squars[0][3];
+//	for (size_t i = 0; i < 4; i++)
+//	{
+//		minX = min(squars[i][0], minX);
+//		minY = min(squars[i][1], minY);
+//		maxX = max(squars[i][2], maxX);
+//		maxY = max(squars[i][3], maxY);
+//	}
+//	vector<int> BSQ = { minX,minY,maxX,maxY };
+//	for (size_t x = minX; x < maxX; x++)
+//	{
+//		for (size_t y = minY; y < maxY; y++)
+//		{
+//			area += (squars[0][0] <= x && squars[0][2] > x && squars[0][1] <= y && squars[0][3] > y) 
+//				|| (squars[1][0] <= x && squars[1][2] > x && squars[1][1] <= y && squars[1][3] > y) 
+//				|| (squars[2][0] <= x && squars[2][2] > x && squars[2][1] <= y && squars[2][3] > y) 
+//				|| (squars[3][0] <= x && squars[3][2] > x && squars[3][1] <= y && squars[3][3] > y);
+//		}
+//	}
+//	cout << area;
+//	return 0;
+//}
