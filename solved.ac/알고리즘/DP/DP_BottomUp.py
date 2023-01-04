ed3a91f406c34586614cd622518fb3b9ddb4ba5c
cong2738 <cong2738@gmail.com>
2023-01-04 16:46:12 +0900

    #bottom_up DP

--- solved.ac/알고리즘/DP/DP_BottomUp.py
+++ solved.ac/알고리즘/DP/DP_BottomUp.py
@@ -1,9 +1,15 @@
+#bottom_up DP
 import sys
 readline = sys.stdin.readline
 N = int(readline())
 stair = [0]+[int(readline()) for _ in range(N)]
-memo = {1:stair[1],2:max(stair[1]+stair[2],stair[2]),3:max(stair[1]+stair[3],stair[2]+stair[3])}
-for i in range(4,N+1):
-    memo[i] = max(memo[i-2]+stair[i], memo[i-3]+stair[i-1]+stair[i])
+memo = {1:stair[1]}
+if N >= 2:
+    memo[2] = max(stair[1]+stair[2],stair[2])
+if N >= 3:
+    memo[3] = max(stair[1]+stair[3],stair[2]+stair[3])
+if N > 3:    
+    for i in range(4,N+1):
+        memo[i] = max(memo[i-2]+stair[i], memo[i-3]+stair[i-1]+stair[i])
 
 print(memo[N])
