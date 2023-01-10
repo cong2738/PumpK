4d0187142e4447ec38e4573a5c705675c51a212b
cong2738 <cong2738@gmail.com>
2023-01-10 18:42:44 +0900

    softeer

--- Softeer/[21년 재직자 대회 예선] 이미지 프로세싱.py
+++ Softeer/[21년 재직자 대회 예선] 이미지 프로세싱.py
@@ -6,7 +6,7 @@ H,W = map(int,readline().split())
 pic = [list(map(int,readline().split())) for _ in range(H)]
 
 def bfs(sx,sy,K):
-    sx-=1;sy-=1
+    sx-=1; sy-=1
     if pic[sy][sx] == K: return []
     move = [(0,1),(0,-1),(1,0),(-1,0)]
     visited = [[False]*W for _ in range(H)]
@@ -17,9 +17,9 @@ def bfs(sx,sy,K):
         pic[y][x] = K
         for dx,dy in move:
             nx,ny = x+dx,y+dy
-            if 0 <= nx < W and 0 <= ny < H and pic[ny][nx] == color and not visited[ny][nx]:
-                visited[ny][nx] = True              
-                Q.append((nx,ny))
+            if not (0 <= nx < W and 0 <= ny < H and pic[ny][nx] == color and not visited[ny][nx]): continue
+            visited[ny][nx] = True
+            Q.append((nx,ny))
 
 for _ in range(int(readline())):
     y,x,K = map(int,readline().split())
