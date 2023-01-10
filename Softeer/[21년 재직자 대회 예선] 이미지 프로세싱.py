5c59ff788f7326479e1a387fd8d5e11a60d4d336
cong2738 <cong2738@gmail.com>
2023-01-10 18:28:29 +0900

    softeer

--- Softeer/[21년 재직자 대회 예선] 이미지 프로세싱.py
+++ Softeer/[21년 재직자 대회 예선] 이미지 프로세싱.py
@@ -1,13 +1,14 @@
 import sys
 from collections import deque
 readline = sys.stdin.readline
+
 H,W = map(int,readline().split())
 pic = [list(map(int,readline().split())) for _ in range(H)]
 
-move = [(0,1),(0,-1),(1,0),(-1,0)]
 def bfs(sx,sy,K):
     sx-=1;sy-=1
     if pic[sy][sx] == K: return []
+    move = [(0,1),(0,-1),(1,0),(-1,0)]
     visited = [[False]*W for _ in range(H)]
     color = pic[sy][sx]
     Q = deque([(sx,sy)])
