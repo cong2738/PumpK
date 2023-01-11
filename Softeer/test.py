ecfa532ef5463d2662f0d3611e219afaf79b8558
cong2738 <cong2738@gmail.com>
2023-01-11 15:13:54 +0900

    민구 뻘짓 도움

--- Softeer/test.py
+++ Softeer/test.py
@@ -2,5 +2,5 @@ def et(string,*L):
     for s in L:
         string = string.replace(s,'')
     return string
-item_option  = [et(s,'value\': ', 'grade\': ', '\'', '{', '}') for s in [s for s in input().split('}, ')]]
+item_option  = [(lambda s:[s[0].split(':'),s[1]])(et(s,'value\': ', 'grade\': ', '\'', '{', '}',' ').split(',')) for s in [s for s in input().split('}, ')]]
 print(*item_option, sep='\n')
