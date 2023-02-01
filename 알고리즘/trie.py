f5b07ec2032e4344359fd14ba1d9fffc399caf0a
cong2738 <cong2738@gmail.com>
2023-02-01 16:18:58 +0900

    트라이 알고리즘 고촬 및 클래스 생성

--- 알고리즘/trie.py
+++ 알고리즘/trie.py
@@ -1,9 +1,12 @@
-#trie(트라이): 여러개의 문자열을 기존 입력을 기반으로 빠르게 탐색하기 위한 트리구조의 자료구조( 메모리를 많이먹는대신 O(log(N)로탐색속도가 매우빠르다. )
+'''
+trie(트라이): 여러개의 문자열을 기존 입력을 기반으로 빠르게 탐색하기 위한 트리구조의 자료구조( 메모리를 많이먹는대신 O(log(N)로탐색속도가 매우빠르다. )
+데이터 접근 편의를 위해 각 노드와 자식 노드관계는 해시맵을 사용한다.
+'''
 class Pumkp_trie:    
     def __init__(self,*words): 
         self.triepush(*words)
     
-    #데이터 접근 편의를 위해 각 노드와 자식 노드관계는 해시맵을 사용한다.
+    #트라이에 단어(들) 푸시
     def triepush(self,*words):
         self.trie = dict()
         for word in set(words):
@@ -12,6 +15,7 @@ class Pumkp_trie:
                 current_dict = current_dict.setdefault(letter, {})
             current_dict[None] = None
     
+    #트라이에 해당단어가 존재하는지 탐색
     def find(self,word):
         current = self.trie
         for c in word:
@@ -19,14 +23,6 @@ class Pumkp_trie:
             else: return False
         return True
 
+    #트라이를 반환
     def get(self): 
         return self.trie
-
-import sys
-readline = sys.stdin.readline
-N,M = map(int,readline().split())
-my_trie = Pumkp_trie(*{readline().rstrip() for _ in range(N)})
-for _ in range(M):
-    my_trie.triepush(readline().rstrip())
-my_trie.triepush(readline().rstrip())
-print(my_trie.find(readline().rstrip()))
