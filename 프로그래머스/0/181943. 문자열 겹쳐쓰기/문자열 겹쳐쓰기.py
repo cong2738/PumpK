e1e06ff002a893525faee9239b687fa062c4a969
cong2738 <45730000+cong2738@users.noreply.github.com>
2025-02-07 16:44:20 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -0,0 +1,8 @@
+def solution(my_string, overwrite_string, s):
+    len_ms = len(my_string)
+    len_os = len(overwrite_string)
+    answer = my_string[:s] + overwrite_string
+    if s+len_os < len_ms:
+        answer += my_string[s+len_os:]
+
+    return answer
