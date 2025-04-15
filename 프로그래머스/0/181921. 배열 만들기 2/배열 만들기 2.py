d94329dde5d5a58d1e2fca114e76d4a9a3ee9e3b
Park Hoyoun <45730000+cong2738@users.noreply.github.com>
2025-04-15 15:12:45 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.06 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,3 +1,13 @@
 def solution(l, r):
-    res = sorted([i for i in range(l,r+1) if not set(str(i)) - {'5','0'}])
-    return res if res else [-1]
+    ret = []
+    def f(lim, val):
+        if lim == 0:
+            ret.append(val)
+            return
+
+        f(lim - 1, val * 10 + 5)
+        f(lim - 1, val * 10)
+
+    f(6, 0)
+
+    return list(i for i in ret if l <= i <= r)[::-1] or [-1]
