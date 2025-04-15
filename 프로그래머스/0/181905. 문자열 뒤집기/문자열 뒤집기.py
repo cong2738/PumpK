67a9d0f6ac81f4bf88e4a2e493a9441af46a9cc0
Park Hoyoun <45730000+cong2738@users.noreply.github.com>
2025-04-15 11:31:16 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,6 +1,6 @@
 def solution(my_string, s, e):
     s1 = my_string[:s]
-    s2 = "".join(reversed(list(my_string[s:e+1])))
+    s2 = my_string[s:e+1][::-1]
     s3 = my_string[e+1:]
     
     return s1+s2+s3
